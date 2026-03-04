13aa9881db9cf3d3f2248f10363776e211941aed
Chalfton <zachjdevore@hotmail.com>
2026-03-04 10:07:18 -0500

    update ShelfStable
    Implemented ShelfStable, if a subclass is inheriting this class then it will always be true so no need for parameters in the constructor. setIsShelfStable() should not be used, the subclass should be changed to not inherit it if needed. Just added it for completeness.

---
+++ ShelfStable.java
@@ -0,0 +1,19 @@
+public class ShelfStable {
+    
+    private boolean isShelfStable;
+
+    public ShelfStable() {
+        isShelfStable = true;
+    }
+
+    public void setIsShelfStable(boolean isShelfStable) {
+        this.isShelfStable = isShelfStable;
+    }
+
+    public boolean getIsShelfStable() {
+        return this.isShelfStable;
+    }
+
+
+
+}
