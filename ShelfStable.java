9740f91a5fa29e79c1a4e0469328d89adf4c4396
Chalfton <zachjdevore@hotmail.com>
2026-03-09 15:28:57 -0400

    Added Javadoc for ShelfStable

--- ShelfStable.java
+++ ShelfStable.java
@@ -1,19 +1,35 @@
+/**
+ * Class purpose: determine if an item is Shelf Stable
+ * @author Zachary Devore
+ * Date: 3/9/26
+ * Section: CSC 311-002
+ */
 public class ShelfStable {
     
+    /** Boolean instance variable to tell weather an item is shelf stable or not */
     private boolean isShelfStable;
 
+    /**
+     * Initializes a new instance is ShelfStable
+     */
     public ShelfStable() {
         isShelfStable = true;
     }
 
+    /**
+     * Sets isShelfStable to the provides boolean param
+     * @param isShelfStable
+     */
     public void setIsShelfStable(boolean isShelfStable) {
         this.isShelfStable = isShelfStable;
     }
 
+    /**
+     * Gets the current value of isShelfStable
+     * @return If it is shelf stable or not
+     */
     public boolean getIsShelfStable() {
         return this.isShelfStable;
     }
-
-
 
 }
