0c11c11b92ee37fa54c528deddd4379d5aaab613
thomaswesley <thomaswesley14@gmail.com>
2026-03-10 08:43:33 -0400

    Created class for ClothingItem

---
+++ ClothingItem.java
@@ -0,0 +1,11 @@
+/**
+ * @author Thomas Wesley
+ * date: 3-10-26
+ * Puprose: class for all clothing items in the store.
+ */
+
+
+public class ClothingItem extends StoreItem{
+
+    //brand name of the item
+    private String brand;
