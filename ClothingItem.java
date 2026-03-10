a75411d5bb38158687d6901e12f9a4107496fc93
thomaswesley <thomaswesley14@gmail.com>
2026-03-10 08:45:00 -0400

    initialized class, created constructors, getters, and setters

--- ClothingItem.java
+++ ClothingItem.java
@@ -9,3 +9,38 @@ public class ClothingItem extends StoreItem{
 
     //brand name of the item
     private String brand;
+
+    //size of the item
+    private String size;
+
+    //color of clothing item
+    private String color;
+
+
+    /**
+     * Constructor for ClothingItem
+     * @param brand
+     * @param size
+     * @param color
+     */
+    public ClothingItem(String brand, String size, String color){
+        this.brand = brand;
+        this.size = size;
+        this.color = color;
+    }
+
+    //getters and setters
+
+
+    public String getBrand() {return brand;}
+
+    public String getSize() {return size;}
+
+    public String getColor() {return color;}
+
+    public void setBrand(String brand) {this.brand = brand;}
+
+    public void setSize(String size) {this.size = size;}
+
+    public void setColor(String color) {this.color = color;}
+}
