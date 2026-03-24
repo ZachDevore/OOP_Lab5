c1e39565baa7a9b052eabe344394c3459731ce9f
MrW3s <thomaswesley14@gmail.com>
2026-03-24 11:14:43 -0400

    created ClothingItem UML

--- WQSDevoreWesleyWilkersonSmith.java
+++ WQSDevoreWesleyWilkersonSmith.java
@@ -515,7 +515,7 @@ public class WQSDevoreWesleyWilkersonSmith {
                    break;
                 case 2:
                     //Sell Inventory
-                    store.sellItem(scanner);
+                    store.sellItem(scanner);//call the sellItem Method
                     break;
                 case 0:
                     flag = true;
